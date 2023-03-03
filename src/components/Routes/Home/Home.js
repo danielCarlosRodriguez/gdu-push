84eabcc253785badd591ce4351e874a0d27206d2
Daniel Rodríguez <danielcarlosrodriguez@gmail.com>
2023-03-03 08:27:59 -0300

    modificación en home y estilos

--- src/components/Routes/Home/Home.js
+++ src/components/Routes/Home/Home.js
@@ -1,36 +1,12 @@
 import React from 'react'
-import fondo from "../../../img/fondo.jpg"
-import logoGeant from "../../../img/logos-geant-push-v1.png"
-import logoDisco from "../../../img/logos-disco-push-v1.png"
-import logoDevoto from "../../../img/logos-devoto-push-v1.png"
-import { Link } from 'react-router-dom'
+import fondo from "../../../img/fondo2.png"
 
 export const Home = () => {
   return (
-    <div className="col d-flex flex-column h-sm-100 ">
-    
-        <div className="contenedor-image">
-          <img src={fondo} alt="fondo" className="w-100" />
-          <div className="centrado  text-white">
-            <strong className="fs-0">GDU-Push</strong>
-            <div className="fs-3">
-              Plataforma de notificaciones app personalizadas
-            </div>
-            <div className="mt-5 bg-light py-5 rounded-4">
-              <Link to="/geant">
-                <img src={logoGeant} alt="Logo Géant" className="mx-5" />
-              </Link>
-              <Link to="/disco">
-                <img src={logoDisco} alt="Logo Disco" className="mx-5" />
-              </Link>
-              <Link to="/devoto">
-                <img src={logoDevoto} alt="Logo Devoto" className="mx-5" />
-              </Link>
-            </div>
-          </div>
-        </div>
-        
-    
+    <div className="col d-flex flex-column h-sm-100 bg-dark">
+      <div className="contenedor-image ">
+        <img src={fondo} alt="fondo" className="w-100" />
+      </div>
     </div>
   );
 }
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -21,7 +21,7 @@ export const Sidebar = () => {
   return (
     <div className="col-12 col-sm-3 col-xl-2 px-sm-2 px-0 bg-dark d-flex sticky-top">
       <div className="d-flex flex-sm-column flex-row flex-grow-1 align-items-center align-items-sm-start px-3 pt-2 text-white">
-        <h3 className="ms-3 nombre-gdu">GDU-Push</h3>
+        <h3 className="nombre-gdu ms-md-3 me-md-0 me-2">GDU-Push</h3>
         {/* <span class="fs-5 ms-3 d-none d-sm-inline">
           GDU<span class="">-Push</span>
         </span> */}
@@ -75,7 +75,7 @@ export const Sidebar = () => {
           </li>
         </ul>
 
-        <div className="dropdown py-sm-4 mt-sm-auto ms-auto ms-sm-0 flex-shrink-1 ">
+        <div className="dropdown py-sm-4 mt-sm-auto ms-auto ms-sm-0 flex-shrink-1 fs-7">
           <div
             href="#"
             className="d-flex align-items-center text-white text-decoration-none dropdown-toggle"
@@ -84,7 +84,7 @@ export const Sidebar = () => {
             aria-expanded="false"
           >
             <img
-              className="rounded-circle me-2"
+              className="rounded-circle me-2 "
               src={avatar}
               alt="alt"
               style={{ width: "32px", height: "32px" }}
